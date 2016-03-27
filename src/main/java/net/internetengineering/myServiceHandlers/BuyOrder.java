144f90de296b02712acc7d367960e056b8d82ff9
Hamed Gorjiara <hamed_gorjiara@yahoo.com>
2016-03-27 14:56:06 +0430

    BuildAndDeploy.sh and Selling and Buying stocks are working fine

---
+++ src/main/java/net/internetengineering/myServiceHandlers/BuyOrder.java
@@ -0,0 +1,38 @@
+package main.java.net.internetengineering.myServiceHandlers;
+
+import main.java.net.internetengineering.domain.dealing.BuyingOffer;
+import main.java.net.internetengineering.exception.DataIllegalException;
+import main.java.net.internetengineering.server.StockMarket;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/buy")
+public class BuyOrder extends HttpServlet{
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String id = request.getParameter("id");
+		String instrument = request.getParameter("instrument");
+		String type = request.getParameter("type");
+		BuyingOffer buyingOffer=null;
+		PrintWriter out =response.getWriter();
+		try {
+			Long price = Long.parseLong(request.getParameter("price"));
+			Long quantity = Long.parseLong(request.getParameter("quantity"));
+			buyingOffer = new BuyingOffer(price,quantity,type,id);
+			if(instrument==null || instrument.isEmpty())
+				throw new DataIllegalException("Mismatched Parameters");
+			buyingOffer.validateVariables();
+		} catch (DataIllegalException e) {
+			out.println(e.getMessage());
+			return;
+		}catch(Exception e){
+			out.println("Mismatched Parameters");
+			return;
+		}
+		StockMarket.getInstance().executeBuyingOffer(out,buyingOffer,instrument);
+	}
+
+}
