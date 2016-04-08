45b4b6039085389072d12ca9950eb755d9f8d93a
pFathololumi <p16_f3@yahoo.com>
2016-04-08 16:56:41 +0430

    Added files via upload

--- src/main/java/net/internetengineering/myServiceHandlers/AddCustomer.java
+++ src/main/java/net/internetengineering/myServiceHandlers/AddCustomer.java
@@ -1,33 +1,39 @@
-//package main.java.net.internetengineering.myServiceHandlers;
-//
-//import java.io.IOException;
-//import java.io.PrintWriter;
-//
-//import domain.Customer;
-//
-//public class AddCustomer extends MyServiceHandler{
-//
-//	@Override
-//	public int executeByStatus(PrintWriter out) throws IOException {
-//		// TODO Auto-generated method stub
-//		String id = params.get("id");
-//        String name = params.get("name");
-//        String family = params.get("family");
-//        if(id==null || id.isEmpty() || name==null || name.isEmpty()
-//				|| family==null || family.isEmpty()){
-//			out.println("Mismatched Parameters");
-//			return 404;
-//		}
-//        if (StockMarket.getInstance().containCustomer(id)) {
-//			out.println("Repeated id");
-//			MyLogger.info("ID '"+id+"' is repeated.");
-//		}else{
-//        	StockMarket.getInstance().addNewCustomer(new Customer(id, name, family));
-//        	out.println("New user is added");
-//			MyLogger.info("New User with id '"+id+"' is added");
-//        }
-//
-//		return 200;
-//	}
-//
-//}
+package main.java.net.internetengineering.myServiceHandlers;
+
+import main.java.net.internetengineering.domain.Customer;
+import main.java.net.internetengineering.exception.DataIllegalException;
+import main.java.net.internetengineering.server.StockMarket;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/add")
+public class AddCustomer extends MyHttpServlet{
+
+	public void doMyPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+
+		String id = request.getParameter("id");
+    	String name = request.getParameter("name");
+    	String family = request.getParameter("family");
+    	if(id==null || id.isEmpty() || name==null || name.isEmpty()
+				|| family==null || family.isEmpty()){
+			out.println("Mismatched Parameters");		
+		}
+    	if (StockMarket.getInstance().containCustomer(id)) {
+			out.println("Repeated id");
+			
+		}else{
+       	StockMarket.getInstance().addNewCustomer(new Customer(id, name, family));
+       	out.println("New user is added");
+       }
+	}
+
+	public void doMyGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doMyPost(request,response);
+	}
+}
--- src/main/java/net/internetengineering/myServiceHandlers/DepositHandler.java
+++ src/main/java/net/internetengineering/myServiceHandlers/DepositHandler.java
@@ -1,38 +1,43 @@
-//package myServiceHandlers;
-//
-//import java.io.IOException;
-//import java.io.PrintWriter;
-//
-//import logger.MyLogger;
-//import server.*;
-//import domain.dealing.TransactionType;
-//
-//public class DepositHandler extends MyServiceHandler{
-//
-//	@Override
-//	public int executeByStatus(PrintWriter out) throws IOException {
-//		// TODO Auto-generated method stub
-//		String id = params.get("id");
-//		Long amount =null;
-//		try{
-//			amount= Long.parseLong(params.get("amount"));
-//			if(id==null ||id.isEmpty())
-//				throw new Exception();
-//		}catch (Exception e){
-//			out.println("Mismatched Parameters");
-//			return 404;
-//		}
-//
-//		if (StockMarket.getInstance().containCustomer(id)){
-//			StockMarket.getInstance().executeFinancialTransaction(id, TransactionType.DEPOSIT,amount);
-//			out.println("Successful");
-//			MyLogger.info("Deposit '"+amount+"' for user ID '"+id+"' ");
-//		}
-//		else {
-//			out.println("Unknown user id");
-//			MyLogger.info("Unknown user id '"+id+"'");
-//		}
-//		return 200;
-//	}
-//
-//}
+package main.java.net.internetengineering.myServiceHandlers;
+
+import main.java.net.internetengineering.domain.dealing.TransactionType;
+import main.java.net.internetengineering.exception.DataIllegalException;
+import main.java.net.internetengineering.server.StockMarket;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/deposit")
+public class DepositHandler extends MyHttpServlet{
+
+	public void doMyPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+
+		String id = request.getParameter("id");
+		Long amount =null;
+		try{
+			amount= Long.parseLong(request.getParameter("amount"));
+			if(id==null ||id.isEmpty())
+				throw new Exception();
+		}catch (Exception e){
+			out.println("Mismatched Parameters");
+		}
+
+		if (StockMarket.getInstance().containCustomer(id)){
+			StockMarket.getInstance().executeFinancialTransaction(id, TransactionType.DEPOSIT,amount);
+			out.println("Successful");
+		}
+		else {
+			out.println("Unknown user id");
+		}
+	}
+	
+
+	public void doMyGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        doMyPost(request,response);
+    }
+
+}
